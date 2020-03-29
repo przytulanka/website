2524267dc677a41e1df73f70cec745a17ac5f6b6
Michal Pawluczuk <mi23chal@gmail.com>
2020-03-29 10:02:37 +0200

    feat: add Layout component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
+/* eslint react/prop-types:0 */
+import React from 'react';
+import Layout from './src/layouts/index';
+
 import './src/assets/styles/global.css';
 import './src/assets/styles/fonts.css';
+
+export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>;
+
+export default wrapPageElement;
