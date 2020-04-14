cfd3d3d28b5a6ca3ab5dd97582428709f4e9f5ed
Michal Pawluczuk <mi23chal@gmail.com>
2020-04-14 09:32:54 +0200

    feat: Card component add title

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Wrapper, Icon, Text } from './styles';
+import { Wrapper, Title, Subtitle, Icon, Text } from './styles';
 
-const Card = ({ color, icon, text, className }) => (
+const Card = ({ color, title, subtitle, icon, text, className }) => (
 	<Wrapper className={className} bg={color}>
+		{title && (
+			<Title>
+				{title}
+				{subtitle && <Subtitle>{subtitle}</Subtitle>}
+			</Title>
+		)}
 		<Icon src={icon} />
 		<Text dangerouslySetInnerHTML={{ __html: text }} />
 	</Wrapper>
@@ -13,11 +19,15 @@ const Card = ({ color, icon, text, className }) => (
 Card.propTypes = {
 	color: PropTypes.string.isRequired,
 	icon: PropTypes.string.isRequired,
+	title: PropTypes.string,
+	subtitle: PropTypes.string,
 	text: PropTypes.string,
 	className: PropTypes.string,
 };
 
 Card.defaultProps = {
+	title: null,
+	subtitle: null,
 	text: null,
 	className: null,
 };
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,17 +1,36 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-	padding: 2em;
+	padding: 1.5em;
 	line-height: 0;
 	border: 0.4em solid ${({ theme, bg }) => theme.color[bg]};
 	border-radius: 1em;
+`;
+
+export const Title = styled.h2`
+	font-size: ${({ theme }) => theme.fontSize.large};
+	line-height: 1.5;
+`;
+
+export const Subtitle = styled.span`
+	display: block;
 `;
 
 export const Text = styled.div`
 	margin: 0;
 	padding: 0;
 	line-height: 1.8;
-	text-align: justify;
+	text-align: left;
+
+	h3 {
+		font-size: 1em;
+		text-align: center;
+	}
+
+	ul {
+		padding: 0 1em;
+		line-height: 2;
+	}
 `;
 
 export const Icon = styled.img`
