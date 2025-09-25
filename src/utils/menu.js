cbdbc18e2676e73da8657cb5b875711f2b4b2aa9
przytulanka <65090122+przytulanka@users.noreply.github.com>
2025-09-25 22:17:46 +0200

    Update formatting in menu.js

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,30 +1,30 @@
 module.exports = [
-	{
-		title: 'tak działamy',
-		to: '!',
-		subMenu: [
-			{ title: 'oferta', to: '/oferta' },
-			{ title: 'plan dnia', to: '/grupy_i_plan_dnia' },
-			{ title: 'kadra', to: '/#crew' },
-			{ title: 'statut', to: '/statut_przedszkola_przytulanka' },
-			{ title: 'regulamin', to: '/regulamin_przedszkola_przytulanka' },
-		],
-	},
-	{
-  	title: 'o nas',
-  	to: '/o_nas/',
-  	subMenu: [
-    	{ title: 'opowiadanie o Przytulance', to: '/o_nas/#story' }
-  		],
-	},
-	{
-		title: 'galeria',
-		to: '!',
-		subMenu: [],
-	},
-	{
-  	title: 'kontakt',
-  	to: '/#contact',   // klik przenosi na kotwicę na stronie głównej
-  	subMenu: [],       // pusta tablica = brak dropdownu
-	},
+  {
+    title: 'tak działamy',
+    to: '!',
+    subMenu: [
+      { title: 'oferta',     to: '/oferta/' },
+      { title: 'plan dnia',  to: '/grupy_i_plan_dnia/' },
+      { title: 'kadra',      to: '/#crew' },
+      { title: 'statut',     to: '/statut_przedszkola_przytulanka/' },
+      { title: 'regulamin',  to: '/regulamin_przedszkola_przytulanka/' },
+    ],
+  },
+  {
+    title: 'o nas',
+    to: '/o_nas/',
+    subMenu: [
+      { title: 'opowiadanie o Przytulance', to: '/o_nas/#story' },
+    ],
+  },
+  {
+    title: 'galeria',
+    to: '/galeria/',
+    subMenu: [],
+  },
+  {
+    title: 'kontakt',
+    to: '/#contact',
+    subMenu: [],
+  },
 ];
