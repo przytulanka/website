d3ecfb0e93c6342e550c9e84149be3abd2da48a0
przytulanka <65090122+przytulanka@users.noreply.github.com>
2025-09-30 10:18:31 +0200

    fix(hero): center hero image with object-fit cover

--- src/views/Hero/styles.js
+++ src/views/Hero/styles.js
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
-	margin: 0 auto;
+  margin: 0 auto;
 `;
 
 export const Image = styled.div`
-	min-height: 30vh;
+  min-height: 30vh;
 
-	img {
-		object-position: right center !important;
-	}
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center center !important;
+    transform-origin: center center !important;
+  }
 `;
