2f26f51e029a32b29e2302bad753b8c9a97b1e80
przytulanka <65090122+przytulanka@users.noreply.github.com>
2025-11-21 20:26:42 +0100

    to samo

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -20,7 +20,7 @@ module.exports = [
 		subMenu: [
 			{ title: 'opowiadanie o Przytulance', to: '/o_nas/#story' },
 			{ title: 'rodzice o Przytulance', to: '/o_nas/#reference' },
-			{ title: 'kadra', to: '/#crew/' },
+			{ title: 'kadra', to: '/#crew' },
 			{ title: 'statut', to: '/statut_przedszkola_przytulanka/' },
 			{ title: 'regulamin', to: '/regulamin_przedszkola_przytulanka/' },
 		],
