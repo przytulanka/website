b24554490ffe93123fc9c0af2777764d7e7758de
przytulanka <65090122+przytulanka@users.noreply.github.com>
2025-11-21 11:44:52 +0100

    Update menuMobile.js
    Add "Rodzice o Przytulance" link to mobile navigation

--- src/utils/menuMobile.js
+++ src/utils/menuMobile.js
@@ -1,10 +1,11 @@
 module.exports = [
-  { title: 'o nas',                 to: '/o_nas/' },
-  { title: 'kadra',                 to: '/#crew' },
-  { title: 'oferta',                to: '/oferta/' },
-  { title: 'plan dnia',             to: '/grupy_i_plan_dnia/' },
-  { title: 'regulamin',             to: '/regulamin_przedszkola_przytulanka/' },
-  { title: 'statut',                to: '/statut_przedszkola_przytulanka/' },
-  { title: 'galeria',               to: '/galeria/' },
-  { title: 'kontakt',               to: '/#contact' },
+  { title: 'o nas', to: '/o_nas/' },
+  { title: 'Rodzice o Przytulance', to: '/o_nas/#reference' }, // <--- DODANE
+  { title: 'kadra', to: '/#crew' },
+  { title: 'oferta', to: '/oferta/' },
+  { title: 'plan dnia', to: '/grupy_i_plan_dnia/' },
+  { title: 'regulamin', to: '/regulamin_przedszkola_przytulanka/' },
+  { title: 'statut', to: '/statut_przedszkola_przytulanka/' },
+  { title: 'galeria', to: '/galeria/' },
+  { title: 'kontakt', to: '/#contact' },
 ];
