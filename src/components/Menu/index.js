35643c57c84dcaf6b8587b5fccd80d08f28bf2f3
przytulanka <65090122+przytulanka@users.noreply.github.com>
2025-11-21 18:17:29 +0100

    Revert "kolejność desktop: oferta, plan dnia, galeria, o nas, kontakt – wymuszamy ją niezależnie od tego, jak przyjdą dane z GraphQL-a"
    This reverts commit d7ae309ebf4e638e0eb0d7a8f5e4905f2d4f9447.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,66 +5,40 @@ import ConditionalLink from 'components/Conditional';
 import Dropdown from './Dropdown';
 import { Wrapper, MenuList, MenuItem, MenuHeader } from './styles';
 
-// Docelowa kolejność pozycji w menu desktop
-const ORDER = ['oferta', 'plan dnia', 'galeria', 'o nas', 'kontakt'];
+const Menu = ({ items, className }) => (
+	<Wrapper className={className}>
+		<MenuList>
+			{items.map(({ node: item }) => {
+	const hasSubmenu = item.subMenu && item.subMenu.length > 0;
 
-const sortItems = items => {
-  const orderMap = new Map(ORDER.map((title, index) => [title, index]));
+	// dropdown pokazujemy TYLKO dla "o nas"
+	const showDropdown = item.title === 'o nas' && hasSubmenu;
 
-  // nie modyfikujemy oryginalnej tablicy – robimy kopię
-  return [...items].sort((a, b) => {
-    const aTitle = a?.node?.title || '';
-    const bTitle = b?.node?.title || '';
+	return (
+		<MenuItem key={item.title}>
+			<MenuHeader
+				as={ConditionalLink}
+				to={item.to}
+			>
+				{item.title}
+			</MenuHeader>
 
-    const aIndex = orderMap.get(aTitle) ?? 999;
-    const bIndex = orderMap.get(bTitle) ?? 999;
-
-    return aIndex - bIndex;
-  });
-};
-
-const Menu = ({ items, className }) => {
-  const orderedItems = sortItems(items);
-
-  return (
-    <Wrapper className={className}>
-      <MenuList>
-        {orderedItems.map(({ node: item }) => {
-          // dodatkowe zabezpieczenie na wypadek undefined
-          if (!item) return null;
-
-          const hasSubmenu =
-            Array.isArray(item.subMenu) && item.subMenu.length > 0;
-
-          // dropdown pokazujemy TYLKO dla "o nas"
-          const showDropdown = item.title === 'o nas' && hasSubmenu;
-
-          return (
-            <MenuItem key={item.title}>
-              <MenuHeader
-                as={ConditionalLink}
-                to={item.to}
-              >
-                {item.title}
-              </MenuHeader>
-
-              {showDropdown && <Dropdown submenu={item.subMenu} />}
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Wrapper>
-  );
-};
+			{showDropdown && <Dropdown submenu={item.subMenu} />}
+		</MenuItem>
+	);
+})
+}
+		</MenuList>
+	</Wrapper>
+);
 
 Menu.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  items: PropTypes.arrayOf(PropTypes.object).isRequired,
-  className: PropTypes.string,
+	items: PropTypes.arrayOf(PropTypes.object).isRequired,
+	className: PropTypes.string,
 };
 
 Menu.defaultProps = {
-  className: null,
+	className: null,
 };
 
 export default Menu;
