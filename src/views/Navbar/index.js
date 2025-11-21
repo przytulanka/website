9dbe09872ccfa4674d994567079ef9860c435094
przytulanka <65090122+przytulanka@users.noreply.github.com>
2025-11-21 17:18:38 +0100

    src/views/Navbar fix. use only utils/menu.js

--- src/views/Navbar/index.js
+++ src/views/Navbar/index.js
@@ -1,78 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 
 import Logo from 'components/Logo';
 import Menu from 'components/Menu';
 import MenuMobile from 'components/MenuMobile';
 import Burger from 'components/Burger';
+
+import menuItems from 'utils/menu';
+import menuMobileItems from 'utils/menuMobile';
+
 import {
-	Wrapper,
-	Container,
-	StyledLogo,
-	StyledMenu,
-	StyledBurger,
-	BurgerMenu,
+  Wrapper,
+  Container,
+  StyledLogo,
+  StyledMenu,
+  StyledBurger,
+  BurgerMenu,
 } from './styles';
 
 const Navbar = () => {
-	const { allMenu, site, logo } = useStaticQuery(graphql`
-    {
-      allMenu {
-        edges {
-          node {
-            title
-            to
-            subMenu {
-              title
-              to
-            }
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          menuMobile {
-            title
-            to
-          }
-        }
-      }
-      logo: file(relativePath: { eq: "logo.svg" }) {
-        publicURL
-      }
-    }
-  `);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const { menuMobile } = site.siteMetadata;
-	const [isOpen, setIsOpen] = useState(false);
+  const disableBurger = () => {
+    setIsOpen(false);
+  };
 
-	const disableBurger = () => {
-		setIsOpen(false);
-	};
+  const toggleBurger = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const toggleBurger = () => {
-		setIsOpen(!isOpen);
-	};
+  useEffect(() => {
+    window.addEventListener('orientationchange', disableBurger);
+    return () => {
+      window.removeEventListener('orientationchange', disableBurger);
+    };
+  }, []);
 
-	useEffect(() => {
-		window.addEventListener('orientationchange', disableBurger);
-		return () => {
-			window.removeEventListener('orientationchange', disableBurger);
-		};
-	}, []);
+  return (
+    <Wrapper id="navbar">
+      <Container>
+        <StyledLogo as={Logo} to="/" />
+        
+        {/* desktop menu korzysta ZAMIAST GraphQL → z utils/menu */}
+        <StyledMenu as={Menu} items={menuItems} />
 
-	return (
-		<Wrapper id="navbar">
-			<Container>
-				<StyledLogo as={Logo} to="/" image={logo.publicURL} />
-				<StyledMenu as={Menu} items={allMenu.edges} />
-				<StyledBurger as={Burger} click={toggleBurger} isOpen={isOpen} />
-				<BurgerMenu isOpen={isOpen}>
-					<MenuMobile items={menuMobile} click={toggleBurger} />
-				</BurgerMenu>
-			</Container>
-		</Wrapper>
-	);
+        <StyledBurger as={Burger} click={toggleBurger} isOpen={isOpen} />
+        <BurgerMenu isOpen={isOpen}>
+
+          {/* mobile menu korzysta z utils/menuMobile */}
+          <MenuMobile items={menuMobileItems} click={toggleBurger} />
+
+        </BurgerMenu>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
