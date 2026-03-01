bc71b67078c361a147c7e03aaccc6200f7de145a
przytulanka <65090122+przytulanka@users.noreply.github.com>
2026-03-01 18:44:09 +0100

    Update index.js
    dodanie plakatu o rekrutacji

--- src/views/Newsfeed/index.js
+++ src/views/Newsfeed/index.js
@@ -1,45 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Slider from 'components/Slider';
 import { uppercaseFirstChar } from 'utils';
 import { SectionWrapper } from 'components/Share';
-import { Wrapper, SliderWrapper } from './styles';
+import { Wrapper, SliderWrapper, PosterLink } from './styles';
 
 const Newsfeed = ({ id }) => {
-	const { newsfeed } = useStaticQuery(graphql`
+	const { newsfeed, poster } = useStaticQuery(graphql`
     {
       newsfeed: markdownRemark(frontmatter: { type: { eq: "pageNewsfeed" } }) {
         frontmatter {
-          color
-          images {
-            id
-            childImageSharp {
-              fluid(maxWidth: 860, quality: 50) {
-                ...GatsbyImageSharpFluid
-              }
-            }
+          }
+        }
+      }
+      poster: file(name: { eq: "nowa_rekrutacja" }) {
+        childImageSharp {
+          fluid(maxWidth: 900, quality: 80) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
     }
   `);
 
-	const { color, images } = newsfeed.frontmatter;
-	if (!images.length) return null;
 
 	return (
 		<Wrapper as={SectionWrapper} id={id} bg={uppercaseFirstChar(color)}>
+			{poster && (
+				<SliderWrapper>
+					<PosterLink href="https://www.przedszkoleprzytulanka.pl/#contact">
+						<Img
+							fluid={poster.childImageSharp.fluid}
+							alt="Nowa rekrutacja - Przedszkole Przytulanka"
+						/>
+					</PosterLink>
+				</SliderWrapper>
+			)}
 			<SliderWrapper>
 				<Slider images={images} type="newsfeed" />
 			</SliderWrapper>
-		</Wrapper>
-	);
-};
-
-Newsfeed.propTypes = {
-	id: PropTypes.string.isRequired,
-};
-
-export default Newsfeed;
